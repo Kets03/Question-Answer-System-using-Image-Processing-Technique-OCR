b2ebd3126a7a4c02ff0a562d7034d8c2195cedf9
Ketaki Narkhede <narkhedeketakij@gmail.com>
2024-09-16 22:15:52 +0530

    Delete RDBMS Project directory

--- RDBMS Project/img_processing.py
+++
@@ -1,7 +0,0 @@
-import pytesseract as tess
-import PIL
-
-myconfig = r"--psm 8 --oem 3"
-def img_to_string(img):    
-    text = tess.image_to_string(PIL.Image.open(img), config = myconfig)
-    return text
